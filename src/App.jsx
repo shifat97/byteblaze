75a621f5828da195f022b507dafe88ae31ea380a
shifat97 <shifatbinreza2@gmail.com>
2024-05-25 21:34:07 +0600

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Hamburger from "./components/Nav/Hamburger";
 import Navbar from "./components/Nav/Navbar";
 import Footer from "./components/Footer/Footer.jsx";
+import Hero from "./components/Hero/Hero.jsx";
 
 function App() {
     return (
@@ -14,8 +15,11 @@ function App() {
                     <Hamburger></Hamburger>
                 </div>
             </div>
+            <div className="min-h-[calc(100vh-116px)]">
+                <Hero/>
+            </div>
             <div>
-                <Footer />
+                <Footer/>
             </div>
         </div>
     );
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+import {Link} from "react-router-dom";
+import background from "../../assets/wave.svg";
+
+export default function Hero() {
+    const backgroundStyle = {
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+        backgroundPosition: "bottom",
+    }
+
+    return (
+        <div style={backgroundStyle} className="flex flex-col items-center justify-center gap-4 min-h-[calc(100vh-116px)]">
+            <div>
+                <h1 className="text-3xl lg:text-4xl font-bold">Welcome to <span className="bg-gradient-to-r from-primary
+                 via-blue-500 to-secondary bg-300% text-transparent bg-clip-text animate-gradient">ByteBlaze</span></h1>
+            </div>
+            <div>
+                <p className="text-center max-w-[512px]">
+                    ByteBlaze is the bridge between the complex world of technology and the curious minds eager to
+                    understand it
+                </p>
+            </div>
+            <div className="flex gap-2">
+                <Link to="/blogs">
+                    <button type="button"
+                            className="px-8 py-3 font-semibold border rounded dark:border-purple-800 dark:text-gray-800
+                             hover:bg-purple-600 hover:text-white">Read Blogs
+                    </button>
+                </Link>
+                <Link to="/bookmarks">
+                    <button type="button"
+                            className="px-8 py-3 font-semibold border rounded dark:border-purple-800 dark:text-gray-800
+                            hover:bg-purple-600 hover:text-white">Bookmarks
+                    </button>
+                </Link>
+            </div>
+        </div>
+    )
+}
